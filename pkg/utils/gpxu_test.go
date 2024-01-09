cb96dab5ca8a48f7bce72eb6d6ec2d61dd71b6ab
beer <beer5214@126.com>
2024-01-09 10:55:11 +0800

    add test

--- pkg/utils/gpxu_test.go
+++ pkg/utils/gpxu_test.go
@@ -1,6 +1,9 @@
 package utils
 
-import "testing"
+import (
+	"github.com/tkrajina/gpxgo/gpx"
+	"testing"
+)
 
 func TestGetDate(t *testing.T) {
 	r, err := ParseGpxData([]string{
@@ -21,4 +24,32 @@ func TestGpxtract(t *testing.T) {
 	}
 	gpx := r[0]
 	t.Log(gpx)
+}
+
+func TestName(t *testing.T) {
+	f, _ := gpx.ParseFile("/tmp/test/result.gpx")
+	for _, track := range f.Tracks {
+		for _, segment := range track.Segments {
+			for _, point := range segment.Points {
+				if point.Longitude < 100 {
+					t.Logf("%v,%v", point.Longitude, point.Latitude)
+					t.Log(point)
+				}
+			}
+		}
+	}
+}
+
+func TestDistance(t *testing.T) {
+	p1 := gpx.Point{
+		Latitude:  18.4400882721,
+		Longitude: 11,
+	}
+	p2 := gpx.Point{
+		Latitude:  18.4400882721,
+		Longitude: 110.3556594849,
+	}
+
+	distance := gpx.Distance2D(p1.Latitude, p1.Longitude, p2.Latitude, p2.Longitude, false)
+	t.Log(distance)
 }
